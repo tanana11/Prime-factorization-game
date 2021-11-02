893f439bec13760ab5a95c457bc15530c5ed6741
ordinary-science <92210960+ordinary-science@users.noreply.github.com>
2021-11-02 17:17:56 +0900

    Update script.js

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ let number = 1;
 
 function mult(n){
   number *= n;
-  answer.innerHTML += n + "*";
+  answer.innerHTML += n + "";
   console.log(number);
 }
 
@@ -64,7 +64,7 @@ document.getElementById("next").addEventListener('click', () => {
   if (number == question.innerHTML){
     alert("atari!");
     number = 1;
-    answer.innerHTML = "";
+    answer.innerHTML = "×";
     question_number();
     answered_number += 1;
     answered. innerHTML = answered_number;
